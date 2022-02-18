f8034e45b4fcf9ed5167b8374af0c314535818a9
Nkoech123 <nicholask320@gmail.com>
2022-02-17 21:29:43 -0800

    models

--- chat/urls.py
+++ chat/urls.py
@@ -1,4 +1,13 @@
-from django.urls import path, include
+from django.urls import path
+
+from . import views
+
+urlpatterns=[
+    path('', views.index , name= "index"),
+    path('<str:room_name>/', views.room, name='room'),
+]
+
+
 
 
 
