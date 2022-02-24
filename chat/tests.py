0349314fa1ade0070c905b4fac1980990ed35f9e
Nkoech123 <nicholask320@gmail.com>
2022-02-23 16:38:43 -0800

    users

--- chat/tests.py
+++ chat/tests.py
@@ -1,9 +1,9 @@
+'''
 from django.test import TestCase
 from channels.testing import HttpCommunicator
 from consumers import ChatRoomConsumer
 
 
-'''Testing Consumers'''
 
 
 
@@ -11,7 +11,9 @@ from consumers import ChatRoomConsumer
 
 
 
-'''Testing Views-let's test the views'''
+
+Testing Views-let's test the views
+'''
 
 
 
